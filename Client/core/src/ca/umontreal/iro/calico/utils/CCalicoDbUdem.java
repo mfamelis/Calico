f4c3719cd200d01a8dcbb295ecb797bd6a172fee
Bechir Belkahla <bechir.belkahla@esprit.tn>
2020-06-23 15:52:34 -0400

    Adding alloy api

--- Client/core/src/ca/umontreal/iro/calico/utils/CCalicoDbUdem.java
+++ Client/core/src/ca/umontreal/iro/calico/utils/CCalicoDbUdem.java
@@ -1,6 +1,7 @@
 package ca.umontreal.iro.calico.utils;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
 
 import com.sun.org.apache.xerces.internal.dom.NodeImpl;
 import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
@@ -17,11 +18,29 @@ import calico.components.CGroup;
 import calico.controllers.CCanvasController;
 import calico.controllers.CConnectorController;
 import calico.controllers.CGroupController;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.alloy4compiler.ast.Attr;
+import edu.mit.csail.sdg.alloy4compiler.ast.Expr;
+import edu.mit.csail.sdg.alloy4compiler.ast.ExprHasName;
+import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
+import edu.mit.csail.sdg.alloy4compiler.ast.Sig.PrimSig;
+import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
 import it.unimi.dsi.fastutil.longs.Long2ReferenceAVLTreeMap;
 import it.unimi.dsi.fastutil.longs.Long2ReferenceOpenHashMap;
+import kodkod.ast.Decl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
-public class CCalicoDbUdem {
+
+
+
+public class CCalicoDbUdem  {
 	
 	public static Logger logger = Logger.getLogger(CCalicoDbUdem.class.getName());
 	
@@ -37,6 +56,16 @@ public class CCalicoDbUdem {
 	
 	public static Long headNodeUUID = 0L;
 	public static Long trailNodeUUID = 0L;
+	
+	
+	private static final String FILENAME = "spl.csv";
+
+	private static final String LINE_DELIMITER = "\n";
+	private static final String CSV_DELIMITER = ",";
+
+	private static final String VAR_DECL_KEYWORD = "var";
+	private static final String FEATURE_MODEL_DECL_KEYWORD = "featureModel";
+	private static final String FEATURE_VAR_DELIMITER = ";";
 	
 	
 
@@ -101,8 +130,116 @@ public class CCalicoDbUdem {
 		}
 		
 		System.out.println(clm);
+		try {
+			Acyclic(null);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Err e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
+	public static void Acyclic(CConnector connector) throws Err, FileNotFoundException 
+	{
+	
+		
+		// Chooses the Alloy4 options
+        A4Options opt = new A4Options();
+        opt.solver = A4Options.SatSolver.SAT4J;
+
+        
+        PrimSig node = new PrimSig("Node", Attr.ABSTRACT);
+       
+        // abstract sig Canvas {}
+        PrimSig canvas = new PrimSig("Canvas", Attr.ABSTRACT);
+       
+        // Canvas { nodes: Node }
+        Expr nodes = canvas.addField("nodes", node.setOf());
+        
+        Expr edges = canvas.addField("edges", nodes.any_arrow_some(nodes).setOf());
+        
+        Canvas curCanvas = clm.getCanvas().get(clm.getCanvas().size()-1);
+        
+        java.util.List<PrimSig> listSigNode = new ArrayList<PrimSig>();
+        
+        for (Node n : curCanvas.getNode())
+        {
+        	PrimSig nodeTemp = new PrimSig("Node_"+n.getNode().getUUID(), node, Attr.ONE);
+        	listSigNode.add(nodeTemp);
+        }
+        
+        PrimSig canvas1 = new PrimSig("Canvas1", canvas, Attr.ONE);
+        
+        Expr x = listSigNode.get(0);
+        
+        for (int i=1; i<listSigNode.size(); i++)
+        {
+        	x.plus(listSigNode.get(i));
+        }
+        System.out.print(x);
+        canvas1.addFact(canvas1.join(nodes).equal(x));
+        
+        
+        Node src = curCanvas.getEdge().get(0).getSrc();
+        Node trg = curCanvas.getEdge().get(0).getTarget();
+        int is=0,it=0;
+		for (int i=0; i<listSigNode.size()-1; i++)
+        {
+        	if(listSigNode.get(i).label == "Node_"+src.getNode().getUUID())
+        		is=i;
+        	if(listSigNode.get(i).label == "Node_"+trg.getNode().getUUID())
+        		it=i;
+        	
+        }
+        
+        
+        Expr y = listSigNode.get(is).one_arrow_one(listSigNode.get(it));
+        
+        for (int i=1; i<curCanvas.getEdge().size(); i++)
+        {
+        	Node srctmp = curCanvas.getEdge().get(i).getSrc();
+            Node trgtmp = curCanvas.getEdge().get(i).getTarget();
+            int istmp=0,ittmp=0;
+    		for (int j=0; j<listSigNode.size()-1; j++)
+            {
+            	if(listSigNode.get(j).label == "Node_"+srctmp.getNode().getUUID())
+            		istmp=j;
+            	if(listSigNode.get(j).label == "Node_"+trgtmp.getNode().getUUID())
+            		ittmp=j;
+            	
+            }
+        	
+        	y.plus(listSigNode.get(istmp).one_arrow_one(listSigNode.get(ittmp)));
+        	
+        }
+        canvas1.addFact(canvas1.join(edges).equal(y));
+        Expr r = edges;
+        
+        //Acyclic
+        edu.mit.csail.sdg.alloy4compiler.ast.Decl d = r.join(Sig.UNIV).oneOf("x");     // x is a variable over the domain of r
+        ExprHasName z = d.get();
+        System.out.println(z.in(z.join(r.closure())).not().forAll(d)); // (x !in x.^r) for all x
+        System.out.println("test");
+
+        
+       	
+        
+       
+
+	}
+
+	private static Canvas findCanvas(String name, CalicoLogicalModel clm) {
+		EList<Canvas> knownBoxes =  clm.getCanvas();
+		for (Canvas c : knownBoxes)
+			if (c.toString().trim().equals(name))
+				return c;
+		return null;
+	}
+        
+        
+	
 	
 	public static void getNodesForEdge(long EdgeUUID) {
 		return;
